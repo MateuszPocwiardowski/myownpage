58dc217b28f036be979c073a461b074b3c86877a
Mateusz Pocwiardowski <mpocwiardowski@o2.pl>
2022-09-18 23:15:26 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Carousel from './compontents/Carousel/Carousel'
+
+const App = () => {
+	return (
+		<div className='App'>
+			<Carousel />
+		</div>
+	)
+}
+
+export default App
---
+++ src/assets/Icons/Icons.js
@@ -0,0 +1,112 @@
+/** @jsxImportSource theme-ui */
+
+import { Image } from 'theme-ui'
+
+import HtmlImage from './Images/html.png'
+import CssImage from './Images/css.png'
+import MaterialUIImage from './Images/materialUI.png'
+import ThemeUIImage from './Images/themeUI.png'
+import JavaScriptImage from './Images/javascript.png'
+import ReactImage from './Images/react.png'
+import TypeScriptImage from './Images/typescript.png'
+import ReduxImage from './Images/redux.png'
+import MobXImage from './Images/mobx.png'
+
+export const HtmlIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={HtmlImage}
+		alt='HTML icon'
+	/>
+)
+
+export const CssIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={CssImage}
+		alt='CSS icon'
+	/>
+)
+
+export const MaterialUIIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={MaterialUIImage}
+		alt='MaterialUI icon'
+	/>
+)
+
+export const ThemeUIIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={ThemeUIImage}
+		alt='ThemeUI icon'
+	/>
+)
+
+export const JavaScriptIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={JavaScriptImage}
+		alt='JavaScript icon'
+	/>
+)
+
+export const ReactIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={ReactImage}
+		alt='React icon'
+	/>
+)
+
+export const TypeScriptIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={TypeScriptImage}
+		alt='TypeScript icon'
+	/>
+)
+
+export const ReduxIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={ReduxImage}
+		alt='Redux icon'
+	/>
+)
+
+export const MobXIcon = () => (
+	<Image
+		sx={{
+			height: '50%',
+			width: 'auto',
+		}}
+		src={MobXImage}
+		alt='MobX icon'
+	/>
+)
